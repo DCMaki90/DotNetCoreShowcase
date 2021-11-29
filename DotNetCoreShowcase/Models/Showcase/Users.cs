7c9ffb3d7fd47d20e2945f4955b620a86ef2258f
Devin Maki <dcmaki90@gmail.com>
2021-11-28 22:05:52 -0600

    Set up and ran scaffolding on ShowcaseSample.sqlite. Added stubbed out version of connection strings to appsettings. Removed out uneeded null conditional on Address in DotnetCoreShowcase.SampleDB

---
+++ DotNetCoreShowcase/Models/Showcase/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreShowcase.Models.Showcase
+{
+    public partial class Users
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long CreationEpoch { get; set; }
+        public string Email { get; set; }
+        public Guid? AddressId { get; set; }
+
+        public virtual Addresses Address { get; set; }
+    }
+}
