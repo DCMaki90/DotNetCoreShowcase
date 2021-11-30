c0907f636c2153df48e8463ec60fff6f1cefb520
Devin Maki <dcmaki90@gmail.com>
2021-11-29 22:32:06 -0600

    Safety Commit. Reorganized project structure. Added user controller. Added Query GetUserByEmail.

---
+++ DotNetCoreShowcase/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreShowcase.Models.Showcase;
+using DotNetCoreShowcase.Services.User;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreShowcase.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+
+    public class UserController : Controller
+    {
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator) => this._mediator = mediator;
+
+        [HttpGet]
+        public async Task<ActionResult<string>> GetUserByEmail(string email)
+        {
+            Users result;
+            try
+            {
+                result = await this._mediator.Send(new GetUserByEmail.Query(email));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ DotNetCoreShowcase/Services/Query/User/GetUserByEmail.cs
@@ -0,0 +1,36 @@
+﻿using DotNetCoreShowcase.Models.Showcase;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetCoreShowcase.Services.User
+{
+    public class GetUserByEmail
+    {
+        public class Query : IRequest<Users>
+        {
+            public string Email { get; set; }
+            public Query(string email)
+            {
+                this.Email = email;
+            }
+        }
+        public class QueryHandler : IRequestHandler<Query, Users>
+        {
+            private ShowcaseGeneratedContext _showcaseGeneratedContext;
+
+            public QueryHandler(ShowcaseGeneratedContext showcaseGeneratedContext)
+            {
+                _showcaseGeneratedContext = showcaseGeneratedContext;
+            }
+
+            public async Task<Users> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await Task.Run(() =>_showcaseGeneratedContext.Users.FirstOrDefault(user => user.Email == request.Email));
+            }
+        }
+    }
+}
